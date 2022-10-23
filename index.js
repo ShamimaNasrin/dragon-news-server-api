c709ee85a3ead5bf15bcf30f348507269ec42224
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-10-23 12:52:32 +0600

    add data

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json');
+
 //to create app
 app.get('/', (req, res) => {
     res.send('News API Running');
 });
+
+//create api for category data
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log('Dragon News Server running on port', port);
